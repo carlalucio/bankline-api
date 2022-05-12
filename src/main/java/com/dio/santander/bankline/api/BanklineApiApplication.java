485883a08d4cf1d2aa5aebe2aceb655cbd73c454
Carla Lucio <103859003+carlalucio@users.noreply.github.com>
2022-05-12 13:58:34 -0300

    Update BanklineApiApplication.java

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -3,7 +3,7 @@ package com.dio.santander.bankline.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-@EntityScan(basePackages = "Model")
+@EntityScan(basePackages = "com.dio.santander.bankline.api.model")
 @SpringBootApplication
 public class BanklineApiApplication {
 

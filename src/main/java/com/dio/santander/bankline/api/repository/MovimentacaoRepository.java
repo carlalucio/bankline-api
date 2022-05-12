dbe0dea70b55395a24890a543c162034ac528264
Carla Lucio <103859003+carlalucio@users.noreply.github.com>
2022-05-12 15:15:01 -0300

    atualização

--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -1,9 +1,10 @@
 package com.dio.santander.bankline.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.dio.santander.bankline.api.model.Movimentacao;
 
+@Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>{
 
 }
